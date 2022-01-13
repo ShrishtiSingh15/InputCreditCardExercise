4141047c79622464d49c38da7c36fe1f60995840
ShrihtiSingh15 <shrishti.singh_cs19@gla.ac.in>
2022-01-13 23:34:42 +0530

    commit

---
+++ app/src/main/java/com/example/inputcreditcardexercise/MainActivity.java
@@ -0,0 +1,30 @@
+package com.example.inputcreditcardexercise;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    Button b1, b2, b3, b4, b5, b6;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        b1 = (Button)findViewById(R.id.button);
+        b2 = (Button)findViewById(R.id.button2);
+        b3 = (Button)findViewById(R.id.button3);
+        b4 = (Button)findViewById(R.id.button4);
+        b5 = (Button)findViewById(R.id.button5);
+        b6 = (Button)findViewById(R.id.button6);
+        b6.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainActivity.this, "Payment Successfull", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+}
